fb449c6c67fbeed8f7f74bd8a55ed5b86f4a0ee2
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2022-07-13 22:47:03 -0600

    add lightgallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 import "./navbar.js";
+import lightGallery from 'lightgallery';
+import lgFullscreen from 'lightgallery/plugins/fullscreen';
+import lgZoom from 'lightgallery/plugins/zoom';
+
+lightGallery(document.getElementById('gallery'), {
+  licenseKey: '0000-0000-000-0000',
+  plugins: [lgFullscreen, lgZoom],
+  speed: 250,
+  backdropDuration: 100,
+  mousewheel: true,
+  selector: '.image'
+});
